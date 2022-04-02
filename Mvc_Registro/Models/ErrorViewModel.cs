d9709ea7ac81da890a8c44a3ad34033270e5680c
Thais <thhaishs.silva98@gmail.com>
2022-04-02 09:06:20 -0300

    Adicionar arquivos de projeto.

---
+++ Mvc_Registro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mvc_Registro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
